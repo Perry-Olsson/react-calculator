83dad19083e26a738b0a038f87965fbdcd3775e9
Perry Olsson <olsson.perry@gmail.com>
2021-01-30 15:23:19 -0800

    moved isValidPress function into validateClick hook

--- src/app/Buttons/utils/clearAllAfterEquals.ts
+++
@@ -1,9 +0,0 @@
-import { allClearPress } from "../../../context/operation";
-import { Dispatch, State } from "../../../context/operation/types";
-
-export const clearAllAfterEquals = (
-  { previousEvent }: State,
-  dispatch: Dispatch
-) => {
-  if (previousEvent === "EQUALS") dispatch(allClearPress());
-};
--- src/hooks/useValidateClick.ts
+++ src/hooks/useValidateClick.ts
@@ -1,4 +1,3 @@
-import { isValidPress } from "../app/Buttons/utils/isValidPress";
 import { useOperationDispatch, useOperationState } from "../context/operation";
 import { Dispatch, EventCodes, State } from "../context/operation/types";
 
@@ -14,4 +13,32 @@ export const useValidateClick = (event: EventCodes, value?: string) => {
   };
 
   return runValidators;
+};
+
+export const isValidPress = (
+  state: State,
+  dispatch: Dispatch,
+  event: EventCodes,
+  value?: string
+): boolean => {
+  if (isInvalidOnNoNumber(state, event)) return false;
+  for (const validator of state.validations) {
+    if (!validator({ state, dispatch, event, value })) return false;
+  }
+  return true;
+};
+
+const isInvalidOnNoNumber = (
+  { currentNumber, operation }: State,
+  event: EventCodes
+): boolean => {
+  if (
+    currentNumber ||
+    event === "ALL_CLEAR" ||
+    event === "DECIMAL" ||
+    event === "DIGIT" ||
+    (event === "OPERATOR" && operation.length)
+  )
+    return false;
+  return true;
 };
